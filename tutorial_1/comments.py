5a2224183ce5b406343267dc6e61cc6ec639c3e9
Adam Cwyk <git@adamcwyk.dev>
2022-10-23 10:19:15 +1100

    Add tutorial 1

---
+++ tutorial_1/comments.py
@@ -0,0 +1,10 @@
+# This is a comment, you start a comment by typing the '#' character.
+# A comment with '#' only spans one line, you you need to start each comment line with '#'.
+
+"""
+This is a multi-line comment.
+We can have many lines here.
+
+To start a multi-line comment, you need 3x '"" or "double-quote" catacters.
+You need 3 at the start of the comment block, and 3 at the end.
+"""
---
+++ tutorial_1/hello_world.py
@@ -0,0 +1,17 @@
+# This program is what's known as a "Hello, World!" program.
+# It is the most basic example of a complete program.
+# It outputs the words "Hello, World!" to the console
+
+
+print("hello world");
+
+"""
+Explanation:
+
+The 'print' word is telling the computer to output what is inside the "()".
+In our case, we're telling the computer to output the text "Hello, World!".
+
+The text we're giving our "print" command is called a "string". 
+A string is like a sentence, it can contain many words, letters, numbers, or
+special chatacters like !@#$%.
+"""
